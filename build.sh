7e23020af80ae4edd476082055d6944fc8ff1960
miketvo <ikxplicitie@gmail.com>
2022-05-26 01:25:23 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,76 +7,70 @@
 #   -c (Optional) Clean built files.  Use in conjunction with '-m'
 #
 
-while getopts d:m:hc flag
-do
+while getopts d:m:hc flag; do
   case "$flag" in
-    d) directory=${OPTARG};;
-    m) mode=${OPTARG};;
-    h)
-      echo "Syntax: build.sh -d [ROOT_DIRECTORY] -m [ debug | release ]"
-      echo "Options:"
-      echo "  -d Specify the project's root directory. Use './' if you're already in the project's root."
-      echo "  -m Specify build mode. Accepted values are 'debug' and 'release'"
-      echo "  -c (Optional) Clean built files. Use in conjunction with '-m'"
-      exit 0
-      ;;
-    c) clean=1;;
-    *)
-      echo "Invalid flag '$flag'"
-      exit 1
-      ;;
+  d) directory=${OPTARG} ;;
+  m) mode=${OPTARG} ;;
+  h)
+    echo "Syntax: build.sh -d [ROOT_DIRECTORY] -m [ debug | release ]"
+    echo "Options:"
+    echo "  -d Specify the project's root directory. Use './' if you're already in the project's root."
+    echo "  -m Specify build mode. Accepted values are 'debug' and 'release'"
+    echo "  -c (Optional) Clean built files. Use in conjunction with '-m'"
+    exit 0
+    ;;
+  c) clean=1 ;;
+  *)
+    echo "Invalid flag '$flag'"
+    exit 1
+    ;;
   esac
 done
 
-if [ "" = "${directory}" ];
-then
+if [ "" = "${directory}" ]; then
   echo "Error: No directory provided. Try 'build.sh -h' for more information."
   exit 1
 fi
 
-if [ "" = "${mode}" ];
-then
+if [ "" = "${mode}" ]; then
   echo "Error: No build mode specified. Try 'build.sh -h' for more information."
   exit 1
 fi
 
-if [ ! -d "${directory}" ];
-then
+if [ ! -d "${directory}" ]; then
   echo "Error: '${directory}' doesn't exist or isn't a directory."
   exit 1
 fi
 
 case "$mode" in
-  "debug")
-    if [ "${clean}" -eq 1 ];
-    then
-      echo "[ Clean | Debug ]"
-      echo "cmake --build ./cmake-build-debug --target clean -j 9"
-      cmake --build ./cmake-build-debug --target dsvcol -j 9
-      echo "[ Clean finished ]"
-    else
-      echo "[ Build | Debug ]"
-      echo "cmake --build ./cmake-build-debug --target dsvcol -j 9"
-      cmake --build ./cmake-build-debug --target dsvcol -j 9
-      echo "[ Build finished ]"
-    fi
-    ;;
-  "release")
-    if [ "${clean}" -eq 1 ];
-        then
-          echo "[ Clean | Release ]"
-          echo "cmake --build ./cmake-build-release --target clean -j 9"
-          cmake --build ./cmake-build-release --target dsvcol -j 9
-          echo "[ Clean finished ]"
-        else
-          echo "[ Build | Release ]"
-          echo "cmake --build ./cmake-build-release --target dsvcol -j 9"
-          cmake --build ./cmake-build-release --target dsvcol -j 9
-          echo "[ Build finished ]"
-        fi
-    ;;
-  *)
-    echo "Invalid build mode '$mode'"
-    exit 1
-    ;;
+"debug")
+  if [ "${clean}" -eq 1 ]; then
+    echo "[ Clean | Debug ]"
+    echo "cmake --build ./cmake-build-debug --target clean -j 9"
+    cmake --build ./cmake-build-debug --target dsvcol -j 9
+    echo "[ Clean finished ]"
+  else
+    echo "[ Build | Debug ]"
+    echo "cmake --build ./cmake-build-debug --target dsvcol -j 9"
+    cmake --build ./cmake-build-debug --target dsvcol -j 9
+    echo "[ Build finished ]"
+  fi
+  ;;
+"release")
+  if [ "${clean}" -eq 1 ]; then
+    echo "[ Clean | Release ]"
+    echo "cmake --build ./cmake-build-release --target clean -j 9"
+    cmake --build ./cmake-build-release --target dsvcol -j 9
+    echo "[ Clean finished ]"
+  else
+    echo "[ Build | Release ]"
+    echo "cmake --build ./cmake-build-release --target dsvcol -j 9"
+    cmake --build ./cmake-build-release --target dsvcol -j 9
+    echo "[ Build finished ]"
+  fi
+  ;;
+*)
+  echo "Invalid build mode '$mode'"
+  exit 1
+  ;;
 esac
