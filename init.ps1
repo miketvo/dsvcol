c0b418f74a8f72217fbcd4e91fcd016de46cce17
miketvo <ikxplicitie@gmail.com>
2022-05-26 01:45:21 +0700

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,7 +1,9 @@
 # Initialize the project with the following CMake configurations
 
-Write-Output "\ncmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_C_COMPILER=gcc -G Ninja -S .\ -B ./cmake-build-debug"
+Write-Output
+Write-Output "cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_C_COMPILER=gcc -G Ninja -S .\ -B ./cmake-build-debug"
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_C_COMPILER=gcc -G Ninja -S ./ -B ./cmake-build-debug
 
-Write-Output "\ncmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_C_COMPILER=gcc -G Ninja -S .\ -B ./cmake-build-release"
+Write-Output
+Write-Output "cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_C_COMPILER=gcc -G Ninja -S .\ -B ./cmake-build-release"
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_C_COMPILER=gcc -G Ninja -S ./ -B ./cmake-build-release
