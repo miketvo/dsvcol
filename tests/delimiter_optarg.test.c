2cf17fffdc92a001ed4f72e267db49cc694011ee
miketvo <ikxplicitie@gmail.com>
2022-05-26 21:13:30 +0700

    More robust delimiter_optarg.test.c

--- tests/delimiter_optarg.test.c
+++ tests/delimiter_optarg.test.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include "../include/delimiter_optarg.h"
 
+
 int main() {
     const char *TEST_CASES[] = {
             "abc",
@@ -14,7 +15,7 @@ int main() {
             "aabb",
             "aaab",
             "aaaa",
-            ""
+            ""  // This signals the end of the array. DO NOT REMOVE.
     };
 
     const size_t TEST_SIZES[] = {  // Not including terminating character '\0'
@@ -24,7 +25,7 @@ int main() {
             4,
             4,
             4,
-            0
+            0  // This signals the end of the array. DO NOT REMOVE.
     };
 
     const char *TEST_ASSERTS[] = {
@@ -33,12 +34,12 @@ int main() {
             "ab",
             "ab",
             "a",
-            ""
+            ""  // This signals the end of the array. DO NOT REMOVE.
     };
 
     char *buffer;
     size_t i = 0;
-    while (strcmp(TEST_CASES[i], "") != 0) {
+    while (strcmp(TEST_CASES[i], "") != 0) {  // Test for the end of the array
         buffer = calloc(TEST_SIZES[i] + 1, sizeof(char));
         delimiter_optarg_nparse(TEST_CASES[i], buffer, TEST_SIZES[i] + 1);
         if (strncmp(buffer, TEST_ASSERTS[i], TEST_SIZES[i]) != 0) {
