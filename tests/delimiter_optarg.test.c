44eaabb7f5d9e09187ea01bdee2df9df634681ea
miketvo <ikxplicitie@gmail.com>
2022-05-26 18:12:53 +0700

    Added test: delimiter_optarg.test.c and restructured .gitignore

---
+++ tests/delimiter_optarg.test.c
@@ -0,0 +1,9 @@
+//
+// Created by miketvo on 5/26/2022.
+//
+#include <stdlib.h>
+#include "../include/delimiter_optarg.h"
+
+int main() {
+    return 0;
+}
--- tests/ischrin.test.c
+++ tests/ischrin.test.c
@@ -4,6 +4,26 @@
 #include <stdlib.h>
 #include "../include/util.h"
 
-int main () {
+int main ()
+{
+    if (!ischrin('a', "abc", 3)) return 1;
+    if (!ischrin('b', "abc", 3)) return 1;
+    if (!ischrin('c', "abc", 3)) return 1;
+    if (ischrin('a', "def", 3)) return 1;
+    if (ischrin('b', "def", 3)) return 1;
+    if (ischrin('c', "def", 3)) return 1;
+
+    char c1 = 'd';
+    char c2 = 'e';
+    char c3 = 'f';
+    char s1[] = "abcdef";
+    char s2[] = "ijkxyz";
+    if (!ischrin(c1, s1, sizeof(s1) / sizeof(s1[0]))) return 1;
+    if (!ischrin(c2, s1, sizeof(s1) / sizeof(s1[0]))) return 1;
+    if (!ischrin(c3, s1, sizeof(s1) / sizeof(s1[0]))) return 1;
+    if (ischrin(c1, s2, sizeof(s2) / sizeof(s2[0]))) return 1;
+    if (ischrin(c2, s2, sizeof(s2) / sizeof(s2[0]))) return 1;
+    if (ischrin(c3, s2, sizeof(s2) / sizeof(s2[0]))) return 1;
+
     return 0;
 }
